a7bfbdf6d9d868b8892756593a17e946df0fe703
yangwb1123 <1832227346@qq.com>
2021-02-27 10:44:30 +0800

    更新

--- src/api/mis.js
+++ src/api/mis.js
@@ -1,7 +1,7 @@
 /*
  * @Author: yanbuw1911
  * @Date: 2020-12-29 11:02:07
- * @LastEditTime: 2021-02-05 08:51:48
+ * @LastEditTime: 2021-02-27 10:36:16
  * @LastEditors: yanbuw1911
  * @Description:
  * @FilePath: /sverp-front/src/api/mis.js
@@ -11,7 +11,10 @@ import { stringify } from 'qs'
 
 const misApi = {
   downloadClient: '/mis/downloadClient',
-  sysUpdate: '/mis/sysUpdate'
+  sysUpdate: '/mis/sysUpdate',
+  startNodeWeb: '/mis/startNodeWeb',
+  stopNodeWeb: '/mis/stopNodeWeb',
+  restartNodeWeb: '/mis/restartNodeWeb'
 }
 
 export const downloadClient = platform =>
@@ -25,5 +28,23 @@ export const downloadClient = platform =>
 export const sysUpdate = () =>
   request({
     url: misApi.sysUpdate,
+    method: 'post'
+  })
+
+export const startNodeWeb = () =>
+  request({
+    url: misApi.startNodeWeb,
+    method: 'post'
+  })
+
+export const stopNodeWeb = () =>
+  request({
+    url: misApi.stopNodeWeb,
+    method: 'post'
+  })
+
+export const restartNodeWeb = () =>
+  request({
+    url: misApi.restartNodeWeb,
     method: 'post'
   })
--- src/views/MIS/Index/index.vue
+++ src/views/MIS/Index/index.vue
@@ -2,26 +2,116 @@
  * @Author: yanbuw1911
  * @Date: 2021-02-05 08:52:49
  * @LastEditors: yanbuw1911
- * @LastEditTime: 2021-02-05 08:55:28
+ * @LastEditTime: 2021-02-27 10:42:31
  * @Description:
  * @FilePath: /sverp-front/src/views/MIS/Index/index.vue
 -->
 <template>
-  <div>
-    <a-button @click="sysUpdate">更新系统</a-button>
+  <div class="container">
+    <a-space>
+      <a-button @click="sysUpdate" :loading="loadingSys">更新系统</a-button>
+      <a-button @click="restartWeb" :loading="loadingWeb">重启 Web 服务</a-button>
+      <a-button @click="restartNode" :loading="loadingNode">重启 Node 服务</a-button>
+      <a-button @click="startNode" :loading="loadingN1">开启 Node 服务</a-button>
+      <a-button @click="stopNode" :loading="loadingN2">停止 Node 服务</a-button>
+    </a-space>
+    <a-divider>响应日志</a-divider>
+    <div class="serv-log"></div>
   </div>
 </template>
 
 <script>
-import { sysUpdate } from '@/api/mis'
+import axios from 'axios'
+import { sysUpdate, startNodeWeb, stopNodeWeb, restartNodeWeb } from '@/api/mis'
 
 export default {
+  data: () => ({
+    loadingSys: false,
+    loadingWeb: false,
+    loadingNode: false,
+    loadingN1: false,
+    loadingN2: false,
+    scrollEl: null
+  }),
   methods: {
-    async sysUpdate () {
-      await sysUpdate().then(res => {
-        console.log(res)
+    getSpentTime (start) {
+      return (new Date().getTime() - start) / 1000
+    },
+    scrollToTop () {
+      this.$nextTick(() => {
+        this.scrollEl.scrollTop = this.scrollEl.scrollHeight
       })
+    },
+    async sysUpdate () {
+      const start = new Date().getTime()
+      this.loadingSys = true
+      await axios.post('http://192.168.123.51:3030/update').then(res => {
+        // await sysUpdate().then(res => {
+        this.scrollEl.innerHTML += res.data
+      })
+      this.scrollEl.innerHTML += `<br>Spent: ${this.getSpentTime(start)}s<br><br>`
+      this.scrollToTop()
+      this.loadingSys = false
+    },
+    async restartWeb () {
+      const start = new Date().getTime()
+      this.loadingWeb = true
+      await axios.post('http://192.168.123.51:3030/nginx/stop').then(res => {
+        this.scrollEl.innerHTML += res.data
+      })
+      await axios.post('http://192.168.123.51:3030/nginx/start').then(res => {
+        this.scrollEl.innerHTML += '<br>' + res.data
+      })
+      this.scrollEl.innerHTML += `<br>Spent: ${this.getSpentTime(start)}s<br><br>`
+      this.scrollToTop()
+      this.loadingWeb = false
+    },
+    async restartNode () {
+      const start = new Date().getTime()
+      this.loadingNode = true
+      await restartNodeWeb().then(res => {
+        this.scrollEl.innerHTML += res.data
+      })
+      this.scrollEl.innerHTML += `<br>Spent: ${this.getSpentTime(start)}s<br><br>`
+      this.scrollToTop()
+      this.loadingNode = false
+    },
+    async startNode () {
+      const start = new Date().getTime()
+      this.loadingN1 = true
+      await startNodeWeb().then(res => {
+        this.scrollEl.innerHTML += res.data
+      })
+      this.scrollEl.innerHTML += `<br>Spent: ${this.getSpentTime(start)}s<br><br>`
+      this.scrollToTop()
+      this.loadingN1 = false
+    },
+    async stopNode () {
+      const start = new Date().getTime()
+      this.loadingN2 = true
+      await stopNodeWeb().then(res => {
+        this.scrollEl.innerHTML += res.data
+      })
+      this.scrollEl.innerHTML += `<br>Spent: ${this.getSpentTime(start)}s<br><br>`
+      this.scrollToTop()
+      this.loadingN2 = false
     }
+  },
+  mounted () {
+    this.scrollEl = document.querySelector('.serv-log')
   }
 }
 </script>
+
+<style lang="less" scoped>
+.container {
+  max-width: 100vw;
+  min-width: 600px;
+
+  .serv-log {
+    height: 500px;
+    min-height: 300px;
+    overflow: auto;
+  }
+}
+</style>
