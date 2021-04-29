dda398f05c07231aebb167d12681462e32ba32fc
mokch <buxueys@gmail.com>
2021-04-29 08:56:05 +0800

    格式化

--- src/views/TPM/index.vue
+++ src/views/TPM/index.vue
@@ -39,381 +39,418 @@
 </template>
 
 <script>
-import {
-  apiMecheInfo,
-  apiSaveMecheInfo,
-  apiSaveFitting,
-  apiFitting,
-} from "@/api/records";
-import XEUtils from "xe-utils";
+import { apiMecheInfo, apiSaveMecheInfo, apiSaveFitting, apiFitting } from '@/api/records'
+import XEUtils from 'xe-utils'
 export default {
-  data() {
+  data () {
     return {
       fittingCount: 0,
-      name: "配件",
+      name: '配件',
       category: true,
       count: [
-        { value: 1, name: "正常", color: "color:RGB(0,191,8)", count: 0 },
-        { value: 2, name: "维修", color: "color:RGB(180,191,8)", count: 0 },
-        { value: 3, name: "保养", color: "color:RGB(13,235,244)", count: 0 },
-        { value: 4, name: "调试", color: "color:RGB(244,144,13)", count: 0 },
-        { value: 5, name: "停用", color: "color:RGB(244,13,29)", count: 0 },
+        {
+          value: 1,
+          name: '正常',
+          color: 'color:RGB(0,191,8)',
+          count: 0
+        },
+        {
+          value: 2,
+          name: '维修',
+          color: 'color:RGB(180,191,8)',
+          count: 0
+        },
+        {
+          value: 3,
+          name: '保养',
+          color: 'color:RGB(13,235,244)',
+          count: 0
+        },
+        {
+          value: 4,
+          name: '调试',
+          color: 'color:RGB(244,144,13)',
+          count: 0
+        },
+        {
+          value: 5,
+          name: '停用',
+          color: 'color:RGB(244,13,29)',
+          count: 0
+        }
       ],
       showTable: true,
       svGridOptions: {
         loading: false,
         data: [],
         params: {
-          originData: [],
+          originData: []
         },
         handleInsert: this.handleInsert,
         handleUpdate: this.handleUpdate,
         editConfig: {
-          activeMethod: this.activeCellMethod,
+          activeMethod: this.activeCellMethod
         },
         keyboardConfig: {
-          editMethod: this.editMethod,
+          editMethod: this.editMethod
         },
         handleSaveOpt: apiSaveMecheInfo,
         columns: [
           {
-            field: "id",
-            title: "ID",
+            field: 'id',
+            title: 'ID',
             width: 60,
             sortable: true,
-            fixed: "left",
+            fixed: 'left'
           },
           {
-            field: "line_num",
-            title: "线号",
+            field: 'line_num',
+            title: '线号',
             width: 90,
             sortable: true,
-            fixed: "left",
+            fixed: 'left',
             editRender: {
-              name: "input",
+              name: 'input'
+            }
+          },
+          {
+            field: 'produc_num',
+            title: '工程号',
+            width: 110,
+            editRender: {
+              name: 'input'
             },
-          },
-          {
-            field: "produc_num",
-            title: "工程号",
-            width: 110,
-            editRender: { name: "input" },
             sortable: true,
-            fixed: "left",
+            fixed: 'left'
           },
           {
-            field: "mache_num",
-            title: "设备编号",
-            editRender: { name: "input" },
+            field: 'mache_num',
+            title: '设备编号',
+            editRender: {
+              name: 'input'
+            },
             width: 110,
-            fixed: "left",
+            fixed: 'left'
           },
           {
-            field: "mache_name",
-            title: "设备名称",
+            field: 'mache_name',
+            title: '设备名称',
             width: 100,
-            fixed: "left",
+            fixed: 'left',
             editRender: {
-              name: "input",
-            },
+              name: 'input'
+            }
           },
           {
-            field: "keeper",
-            title: "管理者",
+            field: 'keeper',
+            title: '管理者',
             editRender: {
-              name: "input",
-            },
-            width: 110,
-            sortable: true,
-            fixed: "left",
-          },
-          {
-            field: "create_time",
-            title: "添加时间",
-            editRender: {
-              name: "input",
+              name: 'input'
             },
             width: 110,
             sortable: true,
-            fixed: "left",
+            fixed: 'left'
           },
           {
-            field: "inspection_cycle",
-            title: "保养周期(天)",
+            field: 'create_time',
+            title: '添加时间',
+            editRender: {
+              name: 'input'
+            },
+            width: 110,
+            sortable: true,
+            fixed: 'left'
+          },
+          {
+            field: 'inspection_cycle',
+            title: '保养周期(天)',
             width: 130,
             editRender: {
-              name: "input",
-            },
+              name: 'input'
+            }
           },
           {
-            field: "status",
-            title: "状态",
+            field: 'status',
+            title: '状态',
             editRender: {
-              name: "$select",
+              name: '$select',
               options: [
                 {
                   value: 1,
-                  label: "正常",
+                  label: '正常'
                 },
                 {
                   value: 2,
-                  label: "维修",
+                  label: '维修'
                 },
                 {
                   value: 3,
-                  label: "保养",
+                  label: '保养'
                 },
                 {
                   value: 4,
-                  label: "调试",
+                  label: '调试'
                 },
                 {
                   value: 5,
-                  label: "停用",
-                },
-              ],
+                  label: '停用'
+                }
+              ]
             },
             width: 100,
             slots: {
-              default: "statusSolt",
-            },
-          },
-        ],
+              default: 'statusSolt'
+            }
+          }
+        ]
       },
       svGridEvents: {
-        refresh: this.refreshTable,
+        refresh: this.refreshTable
       },
       searchLoading: false,
-      showInfoTbl: false,
-    };
+      showInfoTbl: false
+    }
   },
   methods: {
-    fitting() {
-      this.category = !this.category;
+    fitting () {
+      this.category = !this.category
       if (this.category === false) {
-        this.name = "设备";
-        this.svGridEvents.refresh = this.fittingInfo;
+        this.name = '设备'
+        this.svGridEvents.refresh = this.fittingInfo
         this.svGridOptions = {
           loading: false,
           data: [],
           params: {
-            originData: [],
+            originData: []
           },
           handleInsert: this.handleInsert,
           handleUpdate: this.handleUpdate,
           editConfig: {
-            activeMethod: this.activeCellMethod,
+            activeMethod: this.activeCellMethod
           },
           keyboardConfig: {
-            editMethod: this.editMethod,
+            editMethod: this.editMethod
           },
           handleSaveOpt: apiSaveFitting,
           columns: [
             {
-              field: "id",
-              title: "ID",
+              field: 'id',
+              title: 'ID',
               width: 100,
               sortable: true,
-              fixed: "left",
+              fixed: 'left'
             },
             {
-              field: "fitting_name",
-              title: "配件名称",
+              field: 'fitting_name',
+              title: '配件名称',
               width: 130,
-              editRender: { name: "input" },
-              sortable: true,
-              fixed: "left",
-            },
-            {
-              field: "fitting_num",
-              title: "配件数量",
-              editRender: { name: "input" },
-              width: 110,
-              fixed: "left",
-            },
-            {
-              field: "fitting_msg_number",
-              fixed: "left",
-              title: "最低配件数量",
               editRender: {
-                name: "input",
+                name: 'input'
               },
-              width: 130,
+              sortable: true,
+              fixed: 'left'
             },
             {
-              field: "fitting_msg_status",
-              title: "状态",
-              fixed: "left",
+              field: 'fitting_num',
+              title: '配件数量',
               editRender: {
-                name: "$select",
+                name: 'input'
+              },
+              width: 110,
+              fixed: 'left'
+            },
+            {
+              field: 'fitting_msg_number',
+              fixed: 'left',
+              title: '最低配件数量',
+              editRender: {
+                name: 'input'
+              },
+              width: 130
+            },
+            {
+              field: 'fitting_msg_status',
+              title: '状态',
+              fixed: 'left',
+              editRender: {
+                name: '$select',
                 options: [
                   {
                     value: 1,
-                    label: "未发送",
+                    label: '未发送'
                   },
                   {
                     value: -1,
-                    label: "已发送",
-                  },
-                ],
+                    label: '已发送'
+                  }
+                ]
               },
-              width: 100,
+              width: 100
             },
             {
-              field: "fitting_consume_num",
-              title: "已消耗配件数量",
-              editRender: { name: "input" },
+              field: 'fitting_consume_num',
+              title: '已消耗配件数量',
+              editRender: {
+                name: 'input'
+              },
               width: 150,
-              fixed: "left",
-            },
-          ],
-        };
-        this.fittingInfo();
+              fixed: 'left'
+            }
+          ]
+        }
+        this.fittingInfo()
       } else {
-        this.name = "配件";
-        this.svGridEvents.refresh = this.refreshTable;
+        this.name = '配件'
+        this.svGridEvents.refresh = this.refreshTable
         this.svGridOptions = {
           loading: false,
           data: [],
           params: {
-            originData: [],
+            originData: []
           },
           addItem: {
             defaultValue: {
-              status: 1,
+              status: 1
             },
-            focusField: "id",
+            focusField: 'id'
           },
           handleInsert: this.handleInsert,
           handleUpdate: this.handleUpdate,
           editConfig: {
-            activeMethod: this.activeCellMethod,
+            activeMethod: this.activeCellMethod
           },
           keyboardConfig: {
-            editMethod: this.editMethod,
+            editMethod: this.editMethod
           },
           handleSaveOpt: apiSaveMecheInfo,
           columns: [
             {
-              field: "id",
-              title: "ID",
+              field: 'id',
+              title: 'ID',
               width: 100,
               sortable: true,
-              fixed: "left",
+              fixed: 'left'
             },
             {
-              field: "line_num",
-              title: "线号",
+              field: 'line_num',
+              title: '线号',
               width: 140,
               sortable: true,
-              fixed: "left",
+              fixed: 'left',
               editRender: {
-                name: "input",
-              },
+                name: 'input'
+              }
             },
             {
-              field: "produc_num",
-              title: "工程号",
+              field: 'produc_num',
+              title: '工程号',
               width: 110,
-              editRender: { name: "input" },
+              editRender: {
+                name: 'input'
+              },
               sortable: true,
-              fixed: "left",
+              fixed: 'left'
             },
             {
-              field: "mache_num",
-              title: "设备编号",
-              editRender: { name: "input" },
-              width: 110,
-              fixed: "left",
-            },
-            {
-              field: "mache_name",
-              title: "设备名称",
-              width: 100,
-              fixed: "left",
+              field: 'mache_num',
+              title: '设备编号',
               editRender: {
-                name: "input",
+                name: 'input'
               },
+              width: 110,
+              fixed: 'left'
             },
             {
-              field: "keeper",
-              title: "管理者",
+              field: 'mache_name',
+              title: '设备名称',
+              width: 100,
+              fixed: 'left',
               editRender: {
-                name: "input",
+                name: 'input'
+              }
+            },
+            {
+              field: 'keeper',
+              title: '管理者',
+              editRender: {
+                name: 'input'
               },
               width: 120,
               sortable: true,
-              fixed: "left",
+              fixed: 'left'
             },
             {
-              field: "create_time",
-              title: "添加时间",
+              field: 'create_time',
+              title: '添加时间',
               editRender: {
-                name: "input",
+                name: 'input'
               },
               width: 130,
               sortable: true,
-              fixed: "left",
+              fixed: 'left'
             },
             {
-              field: "status",
-              title: "状态",
+              field: 'status',
+              title: '状态',
               editRender: {
-                name: "$select",
+                name: '$select',
                 options: [
                   {
                     value: 1,
-                    label: "正常",
+                    label: '正常'
                   },
                   {
                     value: 2,
-                    label: "维修",
+                    label: '维修'
                   },
                   {
                     value: 3,
-                    label: "保养",
+                    label: '保养'
                   },
                   {
                     value: 4,
-                    label: "调试",
+                    label: '调试'
                   },
                   {
                     value: 5,
-                    label: "停用",
-                  },
-                ],
+                    label: '停用'
+                  }
+                ]
               },
               width: 100,
               sortable: true,
-              fixed: "left",
-            },
-          ],
-        };
-        this.refreshTable();
+              fixed: 'left'
+            }
+          ]
+        }
+        this.refreshTable()
       }
     },
-    async refreshTable(status = "") {
-      this.svGridOptions.loading = true;
+    async refreshTable (status = '') {
+      this.svGridOptions.loading = true
       await apiMecheInfo(status)
-        .then((result) => {
+        .then(result => {
           if (result.code === 0) {
-            if (status === "") {
-              this.count[0].count = 0;
-              this.count[1].count = 0;
-              this.count[2].count = 0;
-              this.count[3].count = 0;
-              this.count[4].count = 0;
+            if (status === '') {
+              // this.count[0].count = 0;
+              // this.count[1].count = 0;
+              // this.count[2].count = 0;
+              // this.count[3].count = 0;
+              // this.count[4].count = 0;
+              this.count.forEach((v, i) => {
+                this.count[i].count = 0
+              })
               result.result.forEach((value, index) => {
-                this.count[value.status - 1].count +=1
-              });
+                this.count[value.status - 1].count += 1
+              })
             }
-            this.svGridOptions.data = result.result.map((e) => {
+            this.svGridOptions.data = result.result.map(e => {
               let row = XEUtils.clone(e, true)
               let status = row.status
               switch (status) {
                 case 1:
                   row.status = '正常'
-                  break;
+                  break
                 case 2:
                   row.status = '维修'
                   break
@@ -427,47 +464,47 @@ export default {
                   row.status = '停用'
                   break
               }
-              return row;
-            });
-            this.svGridOptions.params.originData = this.svGridOptions.data.map((e) =>
-              XEUtils.clone(e)
-            );
+              return row
+            })
+            this.svGridOptions.params.originData = this.svGridOptions.data.map(e => XEUtils.clone(e))
           }
         })
-        .catch(() => {});
-      this.svGridOptions.loading = false;
+        .catch(() => {})
+      this.svGridOptions.loading = false
     },
-    mecheStatus(status) {
-      this.refreshTable({ status });
+    mecheStatus (status) {
+      this.refreshTable({
+        status
+      })
     },
-    async fittingInfo() {
-      this.svGridOptions.loading = true;
+    async fittingInfo () {
+      this.svGridOptions.loading = true
       await apiFitting()
-        .then((result) => {
+        .then(result => {
           if (result.code === 0) {
-            this.svGridOptions.data = result.result.map((e) => {
-              const row = XEUtils.clone(e, true);
-              return row;
-            });
-            this.svGridOptions.params.originData = this.svGridOptions.data.map((e) =>
-              XEUtils.clone(e)
-            );
+            this.svGridOptions.data = result.result.map(e => {
+              const row = XEUtils.clone(e, true)
+              return row
+            })
+            this.svGridOptions.params.originData = this.svGridOptions.data.map(e => XEUtils.clone(e))
           }
         })
-        .catch(() => {});
-      this.svGridOptions.loading = false;
-    },
+        .catch(() => {})
+      this.svGridOptions.loading = false
+    }
   },
-  mounted() {
-    this.refreshTable();
-  },
-};
+  mounted () {
+    this.refreshTable()
+  }
+}
 </script>
+
 <style>
 .box {
   width: 100%;
   padding: 10px 10px;
 }
+
 .box ul li {
   float: left;
   display: block;
