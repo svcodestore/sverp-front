916310fb0d5c7bb1c6bf63381f1325ee979e8728
mokch <buxueys@gmail.com>
2021-05-10 09:51:38 +0800

    添加：判断用户是否具有某个角色权限的helper

--- src/utils/permissions.js
+++ src/utils/permissions.js
@@ -1,3 +1,11 @@
+/*
+ * @Date: 2021-05-08 16:53:20
+ * @LastEditors: Mok.CH
+ * @LastEditTime: 2021-05-10 09:39:05
+ * @FilePath: \sverp-front\src\utils\permissions.js
+ */
+import store from '@/store'
+
 export function actionToObject (json) {
   try {
     return JSON.parse(json)
@@ -5,4 +13,20 @@ export function actionToObject (json) {
     console.log('err', e.message)
   }
   return []
+}
+
+/**
+ * 判断用户是否有某个角色权限
+ * @param {string | Array} roleName
+ * @returns boolean
+ */
+export const hasRole = (roleName) => {
+  if (store.getters.roles.length > 0) {
+    for (const role of store.getters.roles) {
+      if (role.role === roleName || roleName.indexOf(role.role)) {
+        return true
+      }
+    }
+  }
+  return false
 }
