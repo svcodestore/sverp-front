fc74166513003e8fff0cf657060dfeef1cc1bf82
yangwb1123 <1832227346@qq.com>
2021-05-26 17:23:14 +0800

    svgrid 增加行、列拖拽

--- src/views/HRD/KPI/setting/kpi/kpiSetting.vue
+++ src/views/HRD/KPI/setting/kpi/kpiSetting.vue
@@ -2,29 +2,55 @@
  * @Author: yanbuw1911
  * @Date: 2021-05-13 09:51:42
  * @LastEditors: yanbuw1911
- * @LastEditTime: 2021-05-18 16:00:41
+ * @LastEditTime: 2021-05-26 09:28:01
  * @Description: Do not edit
  * @FilePath: /sverp-front/src/views/HRD/KPI/setting/kpi/kpiSetting.vue
 -->
 <template>
   <div>
-    <vxe-table align="center" border>
-      <vxe-table-column field="a1" title="KPI 项目"></vxe-table-column>
-      <vxe-table-column field="a2" title="KPI 名称"></vxe-table-column>
-      <vxe-table-colgroup field="a3" title="使用单位">
-        <vxe-table-column field="a4" title="部门"></vxe-table-column>
-        <vxe-table-column field="a5" title="小组"></vxe-table-column>
-        <vxe-table-column field="a6" title="二级小组"></vxe-table-column>
-        <vxe-table-column field="a7" title="三级小组"></vxe-table-column>
-      </vxe-table-colgroup>
-    </vxe-table>
+    <sv-grid v-bind="gridOptions"></sv-grid>
   </div>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      gridOptions: {
+        align: 'center',
+        columns: [
+          {
+            field: 'a1',
+            title: 'KPI 项目'
+          },
+          {
+            field: 'a2',
+            title: 'KPI 名称'
+          },
+          {
+            title: '使用单位',
+            children: [
+              {
+                field: 'a4',
+                title: '部门'
+              },
+              {
+                field: 'a5',
+                title: '小组'
+              },
+              {
+                field: 'a6',
+                title: '二级小组'
+              },
+              {
+                field: 'a7',
+                title: '三级小组'
+              }
+            ]
+          }
+        ]
+      }
+    }
   }
 }
 </script>
